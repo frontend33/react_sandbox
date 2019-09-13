ebbf8b809aa700882087e2ee0963882c087e6610
Викулов Георгий Александрович <vikulov.georgiy@wildberries.ru>
2019-09-13 18:29:40 +0300

    redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const initialState = 0
+// Если значение state === undefined значит, что еще нет состояния и функция reducer должна вернуть первоначальное состояние state = 0
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INC':
+      return state + 1
+    // Если функция не знает тип дейстия вернуть state без изменений
+    default: 
+      return state
+  }
+}
+/*
+указываем что хотим выполнить действие с типом increment
+Когда работаем с redux, actions самый обычный js объект, у него может быть любая структура,
+но главное чтобы у этого поля было поля была строка, которая указывает тип действия который мы пытаемся соверщить
+*/ 
+let state = reducer(undefined, {})
+state = reducer(state, { type: 'INC' })
+console.log(state) // 1
 
-ReactDOM.render(<App />, document.getElementById('root'));
+state = reducer(state, { type: 'INC' })
+console.log(state) // 2
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
